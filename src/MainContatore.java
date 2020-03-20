8e2d0d67f8372046f829e0702e7b664f64da214e
Ivan Migliore <migliore.ivan.27@gmail.com>
2020-03-20 21:55:58 +0100

    maincontatore

--- src/MainContatore.java
+++ src/MainContatore.java
@@ -1,3 +1,20 @@
-public class MainContatore {
 
+public class MainContatore {
+    public static void main(String[] args){
+
+        contatore c = new contatore(100);
+        boolean e;
+        int lim;
+
+        lim=c.getValueMax();
+
+        for(int i=0;i<lim+2;i++){
+            e=c.isError();
+            c.inc();
+            System.out.println("Il valore attuale è: "+c.getValve);
+            System.out.println("Lo stato interno è: "+e);
+        }
+
+        c.reset();
+    }
 }
