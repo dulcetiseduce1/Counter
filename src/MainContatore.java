7582dfd34b598ff9ec1cd4eacc9e5faab3c0703e
Mirko7222 <20xgame02@gmail.com>
2020-03-20 23:10:06 +0100

    fixed

--- src/MainContatore.java
+++ src/MainContatore.java
@@ -3,21 +3,19 @@ public class MainContatore {
     public static void main(String[] args)
     {
 
-        Contatore c = new Contatore(100);
+        Contatore c = new Contatore(101);
         boolean e;
         int lim;
 
         lim=c.getValueMax();
 
-        for(int i=0;i<lim+2;i++){
-            e=c.isError();
-            c.inc();
+        for(int i=0;i<=lim;i++){
             System.out.println("Il valore attuale è: "+c.getValue());
+            c.inc();
+            e=c.isError();
             System.out.println("Lo stato interno è: "+e);
         }
 
         c.reset();
     }
 }
-
-//main
